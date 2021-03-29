d6ae43f5903168b0eeda2871e1471b982c3dbac9
Simanta <simanta.paul.26@gmail.com>
2021-03-30 03:21:17 +0600

    IPN Controller

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -101,5 +101,5 @@ module.exports.initPayment = async (req, res) => {
 }
 
 module.exports.paymentSuccess = async (req, res) => {
-    res.sendFile(path.join(__basedir + "public/success.html"))
+    res.sendFile(path.join(__basedir + "/public/success.html"))
 }
